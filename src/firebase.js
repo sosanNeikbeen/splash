797932e338c07ef154a6c265f5063fcecbd14c8f
Sosan Neikbeen <sosan.neikbeen@gmail.com>
2021-09-24 10:31:00 +0200

    .env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYXH5QZMK9RiPN9M9hYwwzGiecXNrkIX4",
-  authDomain: "splash-809c8.firebaseapp.com",
-  projectId: "splash-809c8",
-  storageBucket: "splash-809c8.appspot.com",
-  messagingSenderId: "221138124191",
-  appId: "1:221138124191:web:c0f8a470538870a3edd1aa",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
